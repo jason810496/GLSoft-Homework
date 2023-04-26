dab7590281a6d752c664be67ec6901ca7057f366
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-04-27 02:42:55 +0800

    add workflow

--- calculator/vue.config.js
+++ calculator/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/calculator/'
+  publicPath: '/GLSoft-Homework/calculator/'
 })
