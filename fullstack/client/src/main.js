5f8f58c99013a975b357bf1d1fea545cc4c42dce
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-05-22 23:16:41 +0800

    main app

--- fullstack/client/src/main.js
+++ fullstack/client/src/main.js
@@ -1,5 +1,27 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import axios from 'axios';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+import router from './router';
+
+import './style.css';
+
+const app = createApp(App);
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:5001/';  // the FastAPI backend
+
+
+axios.interceptors.response.use(undefined, function (error) {
+    if (error) {
+      const originalRequest = error.config;
+      if (error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+        return router.push('/login')
+      }
+    }
+  });
+  
+  app.use(router);
+
+  app.mount("#app");
