3177c8adfc7c3f0a495cc6943bdee6f2dd743f69
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-05-23 02:46:49 +0800

    fix bearer jwt get auth bug

--- fullstack/client/src/modules/index.js
+++ fullstack/client/src/modules/index.js
@@ -9,52 +9,24 @@ function isAuthenticated() {
     const token = localStorage.getItem('access_token');
     console.log("isAuth localstorage access_token", token);
 
-    // axios.get('http://localhost:5001/whoami',
-    //     {
-    //         headers: { "Authorization": `Bearer ${token}` }
-    //     }).then((response) => {
-    //         console.log(response);
-    //     }).catch((err) => {
-    //         console.log(err);
-    //     });
-
-    // const payload = { 'access_token': token };
-    // axios.post('http://localhost:5001/verify', token , { headers: { 'content-type': 'application/json' } }  )
-    // .then((response) => {
-    //     console.log("check jwt post");
-    //     console.log(response);
-    // }).catch((err) => {
-    //     console.log(err);
-    // });
-
     let auth = false;
 
-    axios.get('http://localhost:5001/whoami',
+    let rep = axios.get('http://localhost:5001/whoami',
         {
             headers: { "Authorization": `Bearer ${token}` , "Accept": 'application/json' , "Access-Control-Allow-Origin": '*' }
         }).then((response) => {
             console.log(response);
+            if (response.status === 200){
+                auth = true;
+            }
             auth = true;
         }).catch((err) => {
             console.log(err);
         });
 
-    // const whoami = axios({
-    //     method: 'GET',
-    //     url: 'http://0.0.0.0:5001/verify',
-    //     responseType: 'json', // responseType 也可以寫在 header 裡面
-    //     headers: {
-    //         Authorization: `Bearer ${token}` ,
-    //         Accept: 'application/json',
-    //         'Access-Control-Allow-Origin': '*',
-    //     }
-    // })
-    // .then(function (response) {
-    //     console.log("isAuth whoami response", response);
-    // })
-    // .catch(function (error) {
-    //     console.log(error);
-    // });
+    console.log("isAuth rep", rep);
+
+    console.log("isAuth auth", auth);
 
     return auth;
 }
--- fullstack/client/src/router/index.js
+++ fullstack/client/src/router/index.js
@@ -2,17 +2,34 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import LoginView from '../views/LoginView.vue'
+import axios from 'axios';
 
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: HomeView,
+        meta: { requiresAuth: false },
     },
     {
         path: '/login',
         name: 'Login',
         component: LoginView,
+        meta: { requiresAuth: false },
+        beforeEnter: (to, from, next ) => {
+            const token = localStorage.getItem('access_token');
+            axios.get('http://localhost:5001/verify',
+            {
+                headers: { "Authorization": `Bearer ${token}` , "Accept": 'application/json' , "Access-Control-Allow-Origin": '*' }
+            }).then((response) => {
+                console.log(response);
+                if (response.status === 200){
+                    next('/profile');
+                }
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
     },
     {
         path: '/profile',
@@ -27,17 +44,20 @@ const router = createRouter({
     routes, // short for `routes: routes`
 })
 
-
 router.beforeEach((to, from) => {
 // instead of having to check every route record with
 // to.matched.some(record => record.meta.requiresAuth)
-    if (to.meta.requiresAuth) {
-    // this route requires auth, check if logged in
-    // if not, redirect to login page.
-        return {
-            path: '/',
-            query: { redirect: to.fullPath },
-        }
+    if (to.meta.requiresAuth ) {
+        const token = localStorage.getItem('access_token');
+        axios.get('http://localhost:5001/verify',
+        {
+            headers: { "Authorization": `Bearer ${token}` , "Accept": 'application/json' , "Access-Control-Allow-Origin": '*' }
+        }).then((response) => {
+            console.log(response);
+        }).catch((err) => {
+            console.log(err);
+            this.$router.push('/login');
+        });
     }
 })
 
